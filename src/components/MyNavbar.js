8fbd2899fcf5b233ff034f68ae43dcafa6419224
wisam-altaher <wisam.al-taher@postnord.com>
2025-12-20 22:48:19 +0100

    linked the logo in the navbar to home

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -5,19 +5,16 @@ function MyNavBar() {
   return (
     <nav class="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
       <div class="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          class="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
+        <Link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+          {/* <img
             src="https://flowbite.com/docs/images/logo.svg"
             class="h-7"
             alt="Flowbite Logo"
-          />
+          /> */}
           <span class="self-center text-xl text-heading font-semibold whitespace-nowrap">
             Wisam
           </span>
-        </a>
+        </Link>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
