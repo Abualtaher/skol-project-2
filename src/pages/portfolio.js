9c71ee1ee5743226e93dd6a0e4772688cdae1ec0
abualtaher <wisamaltaher@gmail.com>
2026-01-07 22:32:46 +0100

    fixing graphiql with contintfull

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,6 +3,24 @@ import Layout from "../components/layout";
 import { Link, graphql, useStaticQuery } from "gatsby";
 
 const portfolioPage = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allContentfulPortfolioItem {
+        nodes {
+          slug
+          title
+        }
+      }
+    }
+  `);
+  if (!data?.allContentfulPortfolio?.edges) {
+    return (
+      <Layout>
+        <h1>Portfolio</h1>
+        <Link to="/">Home</Link>
+      </Layout>
+    );
+  }
   return (
     <Layout>
       <h1>Portfolio</h1>
